bd45a70985304e273a098330830d0441676cb0e5
David Tudury <david.tudury@gmail.com>
2014-10-21 19:56:53 +0000

    new stratagy

--- router.js
+++ router.js
@@ -4,19 +4,22 @@ var routes = require('./routes.json');
 
 module.exports = function(port, hostname) {
     http.createServer(function (req, res) {
-        console.log(req.method + ': ' + req.headers.host + req.url);
-        var route;
-        if (route = routes[req.headers.host]) {
-            var options = {port: route.port, path: req.url, method: req.method};
-            var connector = http.request(options, function (response) {
-                res.writeHeader(response.statusCode, response.headers);
-                response.pipe(res);
-            });
-            req.pipe(connector);
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('Hello World\n');
+        var req_str = [req.method, req.headers.host, req.url].join(' ');
+        console.log(req_str);
+        for (var i = 0; i < routes.length; i++) {
+            var route = routes[i];
+            if (req_str.match(new RegExp(route.regex))) {
+                var options = {port: route.port, path: req.url, method: req.method};
+                var connector = http.request(options, function (response) {
+                    res.writeHeader(response.statusCode, response.headers);
+                    response.pipe(res);
+                });
+                req.pipe(connector);
+                return;
+            }
         }
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World\n');
     }).listen(port, hostname);
     console.log('proxy routes server running at http://%s:%s/', hostname, port);
 };
