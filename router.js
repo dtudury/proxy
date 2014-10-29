17a09b0a28030deed0fa241a354ec8d7fd48fcaf
David Tudury <david.tudury@gmail.com>
2014-10-29 07:01:39 +0000

    wip

--- router.js
+++ router.js
@@ -2,24 +2,21 @@ var http = require('http');
 
 var routes = require('./routes.json');
 
-module.exports = function(port, hostname) {
-    http.createServer(function (req, res) {
-        var req_str = [req.method, req.headers.host, req.url].join(' ');
-        console.log(req_str);
-        for (var i = 0; i < routes.length; i++) {
-            var route = routes[i];
-            if (req_str.match(new RegExp(route.regex))) {
-                var options = {port: route.port, path: req.url, method: req.method};
-                var connector = http.request(options, function (response) {
-                    res.writeHeader(response.statusCode, response.headers);
-                    response.pipe(res);
-                });
-                req.pipe(connector);
-                return;
-            }
+module.exports = function (req, res) {
+    var req_str = [req.method, req.headers.host, req.url].join(' ');
+    console.log(req_str);
+    for (var i = 0; i < routes.length; i++) {
+        var route = routes[i];
+        if (req_str.match(new RegExp(route.regex))) {
+            var options = {port: route.port, path: req.url, method: req.method};
+            var connector = http.request(options, function (response) {
+                res.writeHeader(response.statusCode, response.headers);
+                response.pipe(res);
+            });
+            req.pipe(connector);
+            return;
         }
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello World\n');
-    }).listen(port, hostname);
-    console.log('proxy routes server running at http://%s:%s/', hostname, port);
+    }
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
 };
--- wrapper.js
+++ wrapper.js
@@ -1,4 +1,5 @@
-var timers = require('timers');
-if (process.send) timers.setInterval(function () {process.send("beat");}, 1000);
-var args = process.argv.slice(2);
-require(args.shift()).apply(null, args);
+require('timers').setInterval(function () {process.send("beat");}, 1000);
+require('http').createServer()
+    .listen(process.argv[3])
+    .on("request", require(process.argv[2]))
+    .on("connect", require(process.argv[2]));
